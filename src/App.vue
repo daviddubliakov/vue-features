f2d9cbc4c36a0c7fe165901c93d49729179ac350
daviddubliakov <dubliakov1@gmail.com>
2022-01-02 17:32:15 +0200

    adds loop generated from a number exmple

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
-  <div class="container pt-5">
+  <div class="container pt-5" v-cloak>
     <div class="card center">
       <div v-html="myHtml"></div>
-      <h2 v-text="title"></h2>
+      <!-- <h2 v-text="title"></h2>
       <h2 v-once>{{ title }}</h2>
       <h2 v-pre>{{ title }}</h2>
-      <button class="btn" @click="title = 'I was changed'">Chage title</button>
+      <button class="btn" @click="title = 'I was changed'">Chage title</button> -->
+      <ul class="list">
+        <li class="list-item" v-for="(item, index) in 7" :key="index">
+          {{ item }}
+        </li>
+      </ul>
     </div>
   </div>
 </template>
