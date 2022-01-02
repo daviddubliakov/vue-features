456491a5e9e9855bbdc1f881e004e06d670344a2
daviddubliakov <dubliakov1@gmail.com>
2022-01-02 17:22:21 +0200

    adds v-pre example

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div class="card center">
       <h2 v-text="title"></h2>
       <h2 v-once>{{ title }}</h2>
+      <h2 v-pre>{{ title }}</h2>
       <button class="btn" @click="title = 'I was changed'">Chage title</button>
     </div>
   </div>
